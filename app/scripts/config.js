09b1d0d21f4e2e9da1951fee65f5c6b5c3b7b40b
Thakkar, Aakash <Aakash_Thakkar@intuit.com>
2015-06-15 14:07:46 -0500

    Ability to delete restaurant from list

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,4 +1,4 @@
 angular.module('restaurantPickerUiApp')
     .constant('appConfig', {
-    url: 'http://0.0.0.0:3000/restaurants/'
+    url: 'http://0.0.0.0:3000/restaurant-picker/restaurants/'
 });
--- app/scripts/controllers/all.js
+++ app/scripts/controllers/all.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('restaurantPickerUiApp')
-    .controller('AllCtrl', function ($scope, $http, appConfig, $rootScope) {
+    .controller('AllCtrl', function ($scope, $http, appConfig, $rootScope, $filter) {
 
         $rootScope.currentTab = 'all';
         $scope.restaurants;
@@ -18,6 +18,23 @@ angular.module('restaurantPickerUiApp')
                 })
                 .error(function(error){
                     console.log('Error retreiving all restaurants: ' + error);
+                });
+        };
+
+        $scope.deleteRestaurant = function(restaurantName) {
+
+            console.log('Delete restaurant with name:' + restaurantName);
+            var oneRestaurant = $filter('filter')($scope.restaurants, {name: restaurantName})[0];
+
+            console.log('ID of restaurant to be deleted:' + oneRestaurant._id);
+
+            $http({method: 'DELETE', url:restaurantServiceUrl + oneRestaurant._id})
+                .success(function(response){
+                    console.log('Deleted restaurant with ID: '+ oneRestaurant._id);
+                    $scope.getAllRestaurants();
+                })
+                .error(function(error){
+                    console.log('Error deleting restaurant:' + error);
                 });
         };
     });
